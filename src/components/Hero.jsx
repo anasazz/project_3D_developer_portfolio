a967a924aa44ff6c1fa4bce5b76ef976d114b441
anas <anasazzouz8@gmail.com>
2023-03-27 04:29:33 +0000

    update to dm

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,13 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          <div className='w-5 bg-[#ff6c6d] h-5 rounded-full ' />
+          <div className='w-1 sm:h-80 bg-[#ff6c6d] h-40 ' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Anass AZZOUZ</span>
+            Hi, Welcome to  <span className='text-[#ff6c6d]'>DM AGENCY</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I'm FullStack developer , Mobile Developer, i develop user <br className='sm:block hidden' />
@@ -25,7 +25,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
